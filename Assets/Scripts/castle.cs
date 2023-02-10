c3ec6877400c1a453b28f52bb8943dc90f78ad55
Adrianklobu <123513698+Adrianklobu@users.noreply.github.com>
2023-02-10 12:50:01 +0100

    castle collider

---
+++ Assets/Scripts/castle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class castle : MonoBehaviour
+{
+ private void OnCollisionEnter2D(Collision2D collision)
+   {
+    if (collision.GameObject.name == "paperclip")
+    {
+        Debug.Log("enter");
+    }
+
+   }
+ private void OnCollisionStay2D(Collision2D collision)
+  {
+    if (collision.gameObject.name == "paperclip")
+    {
+        Debug.Log("stay");
+    }
+  }
+  private void OnCollisionExit2D(Collision2D collision)
+  {
+
+    if(collision.gameObject.name == "paperclip")
+    {
+        Debug.Log("exit")
+    }
+  }
+
+}
