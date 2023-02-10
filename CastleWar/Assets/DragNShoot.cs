ae21e21a4eb13d9be3f8c48fac252680f4af379b
dacz-creator <dawid.czech2001@gmail.com>
2023-02-10 12:09:24 +0100

    Paperclip done

--- CastleWar/Assets/DragNShoot.cs
+++ CastleWar/Assets/DragNShoot.cs
@@ -10,6 +10,8 @@ public class DragNShoot : MonoBehaviour
     public Vector2 minPower;
     public Vector2 maxPower;
 
+    TrajectoryLine tl;
+
     Camera cam;
     Vector2 force;
     Vector3 startPoint;
@@ -18,6 +20,7 @@ public class DragNShoot : MonoBehaviour
     private void Start()
     {
         cam = Camera.main;
+        tl = GetComponent<TrajectoryLine>();
     }
 
     private void Update()
@@ -27,6 +30,23 @@ public class DragNShoot : MonoBehaviour
             startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
             startPoint.z = 15;
             Debug.Log(startPoint);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 15;
+            tl.RenderLine(startPoint, currentPoint);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+            tl.EndLine();
         }
     }
     
