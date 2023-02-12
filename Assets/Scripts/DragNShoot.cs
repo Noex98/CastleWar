cfb5d6ecf8ae819b5644ec4aaafbe75829f0ce0b
Johannes <johannes.knickering@hotmail.com>
2023-02-12 05:47:14 +0100

    .

--- Assets/Scripts/DragNShoot.cs
+++ Assets/Scripts/DragNShoot.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragNShoot : MonoBehaviour
-{
+public class DragNShoot : MonoBehaviour {
     public float power = 10f;
     public Rigidbody2D rb;
-
     public Vector2 minPower;
     public Vector2 maxPower;
 
@@ -16,38 +14,53 @@ public class DragNShoot : MonoBehaviour
     Vector2 force;
     Vector3 startPoint;
     Vector3 endPoint;
+    bool hasBeenShot = false;
 
-    private void Start()
-    {
+    private void Start(){
         cam = Camera.main;
         tl = GetComponent<TrajectoryLine>();
     }
 
-    private void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
+    private void HandleMouseInput(){
+        if(Input.GetMouseButtonDown(0)){
             startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-            startPoint.z = 15;
-            Debug.Log(startPoint);
+            startPoint.z = 0;
         }
 
-        if (Input.GetMouseButton(0))
-        {
+        if (Input.GetMouseButton(0)){
             Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-            currentPoint.z = 15;
+            currentPoint.z = 0;
             tl.RenderLine(startPoint, currentPoint);
         }
 
-        if (Input.GetMouseButtonUp(0))
-        {
+        if (Input.GetMouseButtonUp(0)){
             endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-            startPoint.z = 15;
+            startPoint.z = 0;
 
             force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+            Debug.Log(force);
             rb.AddForce(force * power, ForceMode2D.Impulse);
             tl.EndLine();
+            hasBeenShot = true;
         }
     }
-    
+
+    private void HandleStop(){
+        Destroy(gameObject, 0);
+    }
+
+    private void Update(){
+        if (!hasBeenShot){
+            HandleMouseInput();
+        } else {
+            bool hasStoppedMoving = rb.velocity.magnitude == 0;
+            if (hasStoppedMoving){
+                HandleStop();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        Debug.Log("collision");
+    }
 }
--- Assets/Scripts/TrajectoryLine.cs
+++ Assets/Scripts/TrajectoryLine.cs
@@ -3,30 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-public class TrajectoryLine : MonoBehaviour
-{
+public class TrajectoryLine : MonoBehaviour{
     public LineRenderer lr;
 
-    private void Awake()
-    {
+    private void Awake(){
         lr = GetComponent<LineRenderer>();
-
     }
 
-    public void RenderLine(Vector3 startPoint, Vector3 endPoint)
-    {
+    public void RenderLine(Vector3 startPoint, Vector3 endPoint){
         lr.positionCount = 2;
         Vector3[] points = new Vector3[2];
         points[0] = startPoint;
         points[1] = endPoint;
 
         lr.SetPositions(points);
-
     }
 
-    public void EndLine ()
-    {
+    public void EndLine (){
         lr.positionCount = 0;
     }
-
 }
